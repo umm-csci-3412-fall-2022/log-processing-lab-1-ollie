d6f1e989327fc67100a3c598e18a716740d800e1
Ollie Willette <wille667@morris.umn.edu>
2022-09-20 16:23:07 -0500

    Make shellcheck happy

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -4,8 +4,10 @@ SCRATCH=$(mktemp --directory)
 home=$(pwd)
 cd "$1" || exit
 cat "$home/html_components/country_dist_header.html" > country_dist.html
-for f in $(ls -d */); do
-        cat "$f/failed_login_data.txt" | awk '{ print $5 }' >> "$SCRATCH/listOfIP.txt"  # for every file find failed.txt and print ip into temp text file
+for f in */; do
+        [[ -e "$f" ]] || break 
+	awk '{ print $5 }' "$f/failed_login_data.txt" >> "$SCRATCH/listOfIP.txt"  # for every file find failed.txt and print ip into temp text file
+
 done
 sort "$SCRATCH/listOfIP.txt" | join -o 1.2 "$home/etc/country_IP_map.txt"  - | sort | uniq -c | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);">> "country_dist.html"}'
 # sort temp.txt, join that with IP_map.txt but just return the second value (country code) from the first file (IP_map). sort that again because not all ip have same country, count and print country code then #.
--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,8 +4,9 @@ SCRATCH=$(mktemp --directory)
 home=$(pwd)
 cd "$1" || exit
 cat "$home/html_components/username_dist_header.html" > username_dist.html # put header
-for f in $(ls -d */); do
-	cat "$f/failed_login_data.txt" | awk '{ print $4 }' >> "$SCRATCH/listOfNames.txt" # for every dir available, print username ($4) from failedlogindata in temp textfile
+for f in  */; do
+	[[ -e "$f" ]] || break
+	awk '{ print $4 }' "$f/failed_login_data.txt" >> "$SCRATCH/listOfNames.txt" # for every dir available, print username ($4) from failedlogindata in temp textfile
 done
 sort "$SCRATCH/listOfNames.txt" | uniq -c | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);">> "username_dist.html"}'  #f count uniques and put name then number into _dist.html
 cat "$home/html_components/username_dist_footer.html" >> username_dist.html # put footer
